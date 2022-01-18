50b39a7f17123689db3301ba79ff9585ceac5ca8
Dave Wendt <djw5383@gmail.com>
2022-01-17 22:06:03 -0500

    Trying to get the Incorrect and Correct answers to add up to the right score

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your Code Here
+let userName = window.prompt( 'Please enter your name:' )
+let userScore = 0
+
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+   
+    if(userAnswer === question.correctAnswer){
+        console.log ('Correct')
+        window.alert('Correct!')
+    }
+
+        userScore = userScore + 10
+
+    if (userAnswer !== question.correctAnswer){
+        console.log ('Incorrect')
+        window.alert('Incorrect, sorry')
+    }
+        
+    
+}
+
+window.alert('Your score is: ' + userScore)
+
+let games = []
+
+let game = {
+    user: userName,
+    score: userScore
+}
+
+games.push(game)
+
+console.log (games)
